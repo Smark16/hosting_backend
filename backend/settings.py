bff11b4587e4a2279118400a7759c40a2ba17d43
Sengendo Mark <146088416+Smark16@users.noreply.github.com>
2024-07-05 15:36:49 +0300

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-   "http://localhost:5173"
+   "https://institute-application-form.onrender.com"
 ]
 
 

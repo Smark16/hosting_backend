692be513210847b91d5dd73ada3a033395374df6
Smark16 <sengendomark16@gmail.com>
2024-07-05 15:11:08 +0300

    added requirements

--- form_register/serializers.py
+++ form_register/serializers.py
@@ -9,7 +9,7 @@ from django.contrib.auth.password_validation import validate_password
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'date_joined.date']
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'date_joined']
 
 class ObtainSerializer(TokenObtainPairSerializer):
     @classmethod

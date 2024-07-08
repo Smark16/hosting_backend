eb80272a2c6750ea1502b8f707b38d5056f864a4
Smark16 <sengendomark16@gmail.com>
2024-07-08 13:17:17 +0300

    merge

--- backend/settings.py
+++ backend/settings.py
@@ -86,8 +86,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'institute',
+        'USER':'postgres',
+        'PASSWORD':'smark',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 database_url = os.environ.get("DATABASE_URL")

c66c0dcb3ebd558cf94fac176b26ce76c9b29a84
Smark16 <sengendomark16@gmail.com>
2024-07-29 09:41:49 +0300

    merge

--- backend/settings.py
+++ backend/settings.py
@@ -147,6 +147,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
    "https://institute-application-form.onrender.com",
+   "https://dashboard-mis-officer.vercel.app",
 ]
 
 
